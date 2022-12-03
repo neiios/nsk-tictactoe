e4273811ac726a5e39bebea337a5f250ec14c7dc
Igor <68248740+richard96292@users.noreply.github.com>
2022-12-03 20:32:35 +0200

    Add confirmation prompts

--- script.js
+++ script.js
@@ -217,3 +217,20 @@ function loadFromSessionStorage() {
 }
 
 loadFromSessionStorage();
+
+const modals = document.querySelectorAll("[data-modal]");
+
+modals.forEach(function (trigger) {
+  trigger.addEventListener("click", function (event) {
+    event.preventDefault();
+    const modal = document.getElementById(trigger.dataset.modal);
+    modal.classList.add("open");
+    const exits = modal.querySelectorAll(".modal-exit");
+    exits.forEach(function (exit) {
+      exit.addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.classList.remove("open");
+      });
+    });
+  });
+});
