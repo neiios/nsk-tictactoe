3253ce21b719d0b9ddc50100d742d222a9a0374a
Igor <68248740+richard96292@users.noreply.github.com>
2022-12-03 16:24:45 +0200

    Implement more advanced game logic

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const gameContainer = document.querySelector(".game-container");
+const fieldContainer = document.querySelector(".field-container");
 const fieldSize = 3;
+
+let players = [
+  { name: "", score: 0 },
+  { name: "", score: 0 },
+];
 // true - x, false - 0
 let currentPlayer = true;
-
 // track the state of the game
 let gameState = ["", "", "", "", "", "", "", "", ""];
-
 // winning conditions
 const winningConditions = [
   [0, 1, 2],
@@ -20,14 +24,12 @@ const winningConditions = [
 
 // creates fields
 function createFields() {
-  document.querySelector(".game-container").classList.remove("hidden");
-
   for (let i = 0; i < fieldSize * fieldSize; i++) {
     const field = document.createElement("button");
     field.addEventListener("click", handleFieldClick);
     field.id = i;
     field.classList.add("field", "active-field");
-    gameContainer.appendChild(field);
+    fieldContainer.appendChild(field);
   }
 }
 
@@ -40,12 +42,12 @@ function handleFieldClick() {
     this.innerHTML = `<i class="icon fa-regular fa-circle"></i>`;
     gameState[parseInt(this.id)] = "O";
   }
-  currentPlayer = !currentPlayer;
 
   this.removeEventListener("click", handleFieldClick);
   this.classList.remove("active-field");
 
   validateResults();
+  currentPlayer = !currentPlayer;
 }
 
 // remove active class and click handler
@@ -78,8 +80,7 @@ function validateResults() {
 
   // detect winning
   if (won) {
-    alert("You have won!");
-    makeInactive();
+    handleWin();
     return;
   }
 
@@ -87,6 +88,7 @@ function validateResults() {
   let draw = !gameState.includes("");
   if (draw) {
     alert("Draw!");
+    handleDraw();
     makeInactive();
     return;
   }
@@ -94,5 +96,65 @@ function validateResults() {
 
 function getPlayerInfo() {
   document.querySelector(".menu").classList.add("hidden");
+  setPlayerNames();
   createFields();
+  document.querySelector(".game-container").classList.remove("hidden");
+}
+
+function toggleAnotherGameButton() {
+  if (document.querySelector(".another-game")) {
+    document.querySelector(".another-game").remove();
+    const button = document.createElement("button");
+    button.classList.add("reset-game");
+    button.innerText = "Reset game";
+    button.addEventListener("click", resetGame);
+    gameContainer.appendChild(button);
+  } else {
+    document.querySelector(".reset-game").remove();
+    const button = document.createElement("button");
+    button.classList.add("another-game");
+    button.innerText = "Play another game";
+    button.addEventListener("click", resetGame);
+    gameContainer.appendChild(button);
+  }
+}
+
+function handleWin() {
+  alert("You have won!");
+  makeInactive();
+  currentPlayer ? players[0].score++ : players[1].score++;
+  setPlayerNames();
+  toggleAnotherGameButton();
+}
+
+function setPlayerNames() {
+  players[0].name = document.getElementById("player1").value;
+  players[1].name = document.getElementById("player2").value;
+  document.getElementById("scoreboard").innerText =
+    players[0].name +
+    " (X) " +
+    players[0].score +
+    " - " +
+    players[1].score +
+    " " +
+    players[1].name +
+    " (O) ";
+}
+
+function resetGame() {
+  document.querySelectorAll(".field").forEach((field) => field.remove());
+  currentPlayer = true;
+  gameState = ["", "", "", "", "", "", "", "", ""];
+  createFields();
+}
+
+function exitGame() {
+  document.querySelector(".game-container").classList.add("hidden");
+  document.querySelector(".menu").classList.remove("hidden");
+  document.querySelectorAll(".field").forEach((field) => field.remove());
+  players = [
+    { name: "", score: 0 },
+    { name: "", score: 0 },
+  ];
+  currentPlayer = true;
 }
